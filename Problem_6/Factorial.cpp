556a1f2a2503f5e8c2377aebe487ba5c6a5b7a79
hana-cmd <61834896+hana-cmd@users.noreply.github.com>
2024-11-25 14:04:20 +0700

    Update Factorial.cpp

--- Problem_6/Factorial.cpp
+++ Problem_6/Factorial.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 unsigned int factorial (unsigned int n)
 {
-   if (n == 0)
+   if (n == 0) //the formula of the factorial
      return 1;
-   return n * factorial (n - 1);   
+   return n * factorial (n - 1); //use this function to calculate the factorial
 }
 
 int main () {
-    int num = 4;
+    int num = 4; //the value 
 
     cout << "Problem 6 in Responsi";
     cout << "\n======================";
-    cout << "\nEnter NUmber of " << num << " is: "
+    cout << "\nEnter NUmber of " << num << " is: " //the output of the program
          << factorial (num) << endl;
 
   return 0;
