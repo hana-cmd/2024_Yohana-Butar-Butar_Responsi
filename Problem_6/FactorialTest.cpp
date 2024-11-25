ea2745167ee679af0b7c285d12092f78518861d6
hana-cmd <61834896+hana-cmd@users.noreply.github.com>
2024-11-25 14:05:07 +0700

    Update FactorialTest.cpp

--- Problem_6/FactorialTest.cpp
+++ Problem_6/FactorialTest.cpp
@@ -5,11 +5,11 @@ unsigned int factorial (unsigned int n)
 {
    if (n == 0) //the formula of the factorial
      return 1;
-   return n * factorial (n - 1);   
+   return n * factorial (n - 1); //use this function to calculate the factorial
 }
 
 int main () {
-    int num = 4;
+    int num = 4; //the value
 
     cout << "Problem 6 in Responsi";
     cout << "\n======================";
