0591a6a68551256462dbab34eda6a5ba3262efc8
hana-cmd <61834896+hana-cmd@users.noreply.github.com>
2024-11-25 14:02:17 +0700

    Update ReverseStringTest.cpp

--- Problem_2/ReverseStringTest.cpp
+++ Problem_2/ReverseStringTest.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() 
 {
-    string input, reversed; //the formula for reverse the string
+    string input, reversed; 
     
     cout << "Problem 2 in Responsi";
     cout << "\n======================";
