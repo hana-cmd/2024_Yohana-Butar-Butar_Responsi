4056522360fce4785a685f9bd73b0354e7323b73
hana-cmd <61834896+hana-cmd@users.noreply.github.com>
2024-11-25 13:51:51 +0700

    Update ReverseStringTest.cpp

--- Problem_2/ReverseStringTest.cpp
+++ Problem_2/ReverseStringTest.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 int main() 
 {
-    string input = 1234;
-    string reversed; //the formula for reverse the string
+    string input, reversed; //the formula for reverse the string
     
     cout << "Enter the String: " << input; //put the value of the string
     
     reversed = input;
     reverse(reversed.begin(), reversed.end()); //the formula to reverse the string
     
-    unsigned int result = factorial(num); //the output of the program
+    unsigned string result = factorial(num); //the output of the program
 
     // Test condition: Check if the factorial of 4 is 24
-    if (result == 24) {
+    if (result == reversed) {
         cout << "Test Passed" << endl;
     } else {
         cout << "Test Not Passed" << endl;
