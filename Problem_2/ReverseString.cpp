40d355968f1f6c8d45960651704a7e7983cb35da
hana-cmd <61834896+hana-cmd@users.noreply.github.com>
2024-11-25 13:41:45 +0700

    Update ReverseString.cpp

--- Problem_2/ReverseString.cpp
+++ Problem_2/ReverseString.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+int main() 
+{
+    string input, reversed; //the formula for reverse the string
+    
+    cout << "Enter the String: ";
+    cin >> input; //put the value of the string
+    
+    reversed = input;
+    reverse(reversed.begin(), reversed.end()); //the formula to reverse the string
+    
+    cout << "Reversed String: " << reversed << endl; //the output of the program
+    
+    return 0;
+}
